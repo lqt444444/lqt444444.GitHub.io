aa07030793d8dc4f771bdeefecb92262c2c36c39
lqt444444 <liqiuting3333@outlook.com>
2025-08-06 17:43:28 +0800

    fix地图生成，标题，添加文章

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://lqt444444.github.io/",
+	site: "https://blog.342191.xyz",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
